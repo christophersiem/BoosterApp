b4a9b6e32f3332165a441233227ecd5946deff37
“Christopher” <cman1609@gmail.com>
2020-07-02 20:26:45 +0200

    Init spring backend

---
+++ booster-app/src/main/java/de/neuefische/boosterapp/BoosterAppApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.boosterapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoosterAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoosterAppApplication.class, args);
+    }
+
+}
