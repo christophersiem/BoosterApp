deebfa06593f818959b0fcd9804ba6758eca5028
“Christopher” <cman1609@gmail.com>
2020-07-03 21:41:19 +0200

    Delete unused imports

--- booster-app/src/main/java/de/neuefische/boosterapp/db/BoosterDb.java
+++ booster-app/src/main/java/de/neuefische/boosterapp/db/BoosterDb.java
@@ -2,9 +2,8 @@ package de.neuefische.boosterapp.db;
 
 import de.neuefische.boosterapp.model.Booster;
 import de.neuefische.boosterapp.model.BoosterType;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.server.ResponseStatusException;
+
 
 import java.util.ArrayList;
 import java.util.List;
