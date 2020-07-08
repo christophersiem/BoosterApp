01400f3d65a158f71e77c26ee79d1876e933cbaf
“Christopher” <cman1609@gmail.com>
2020-07-08 11:52:25 +0200

    Change footer layout

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,45 +1,27 @@
-import React from "react";
-import FormatListBulletedIcon from '@material-ui/icons/FormatListBulleted';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import InfoIcon from '@material-ui/icons/Info';
 import AddIcon from '@material-ui/icons/Add';
-import {makeStyles} from "@material-ui/core/styles";
-import Grid from "@material-ui/core/Grid";
-import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
+import LibraryBooksIcon from '@material-ui/icons/LibraryBooks';
 
-const useStyles = makeStyles((theme) => ({
-    listIcon: {
-        padding: "15px",
-    },
-    addIcon:{
-        padding: "15px",
-    },
-    helpIcon:{
-        padding: "15px",
 
-    }
+export default function SimpleBottomNavigation() {
+    const [value, setValue] = React.useState(0);
 
-}));
-
-export default function Footer() {
-    const classes = useStyles();
     return (
-        <footer>
-            <Grid
-                container
-                direction="row"
-                justify="space-between"
-                alignItems="center"
-            >
-                <Grid>
-            <FormatListBulletedIcon className={classes.listIcon} fontSize={"large"}/>
-                </Grid>
-                <Grid>
-            <AddIcon className={classes.addIcon}fontSize={"large"}/>
-                </Grid>
-                <Grid>
-                    <HelpOutlineIcon className={classes.helpIcon}fontSize={"large"}/>
-                </Grid>
-            </Grid>
-        </footer>
+        <BottomNavigation
+            value={value}
+            onChange={(event, newValue) => {
+                setValue(newValue);
+            }}
+            showLabels
 
-    )
+        >
+            <BottomNavigationAction label="Boosts" icon={<LibraryBooksIcon />} />
+            <BottomNavigationAction label="New Boost" icon={<AddIcon />} />
+            <BottomNavigationAction label="FAQ" icon={<InfoIcon />} />
+        </BottomNavigation>
+    );
 }
