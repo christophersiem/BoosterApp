89f898df0a63e6752ac8ba491fbe19a7e4912570
“Christopher” <cman1609@gmail.com>
2020-07-15 18:03:50 +0200

    Add LoginPage with PrivateRoute

--- booster-app/src/main/java/de/neuefische/boosterapp/model/BoosterUser.java
+++ booster-app/src/main/java/de/neuefische/boosterapp/model/BoosterUser.java
@@ -9,12 +9,13 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Document(collection = "user")
+@Document(collection = "boosterUser")
 public class BoosterUser {
 
     @Id
     private String username;
     private String password;
+    private String firstName;
     private String role;
 }
 
---
+++ frontend/src/pages/PrivateRoute.js
@@ -0,0 +1,39 @@
+import React, {useContext, useEffect} from "react";
+import {removeJWTToken} from "../utils/jwt-utils";
+import Route from "react-router-dom/es/Route";
+import Redirect from "react-router-dom/es/Redirect";
+import {UserDispatchContext, UserStateContext} from "../context/user/UserContext";
+import {LOGOUT} from "../context/user/UserContextProvider";
+
+function PrivateRoute({ component: Component, ...rest }) {
+    const { authStatus, userData } = useContext(UserStateContext);
+    const dispatch = useContext(UserDispatchContext);
+    useEffect(() => {
+        if (
+            authStatus === 'SUCCESS' &&
+            new Date().getTime() / 1000 >= userData.exp
+        ) {
+            removeJWTToken();
+            dispatch({ type: LOGOUT });
+        }
+    });
+
+    return (
+        <Route
+            {...rest}
+            render={(props) => {
+                if (authStatus !== 'SUCCESS') {
+                    return <Redirect to={'/login'} />;
+                }
+
+                if (new Date().getTime() / 1000 >= userData.exp) {
+                    return <Redirect to={'/login'} />;
+                }
+
+                return <Component {...props} />;
+            }}
+        />
+    );
+}
+
+export default PrivateRoute;
