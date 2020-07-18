2e9835f9e9b8295559653263bd738b6e0eb19ebf
“Christopher” <cman1609@gmail.com>
2020-07-18 12:48:59 +0200

    Edit Usermodel and Service

--- booster-app/src/main/java/de/neuefische/boosterapp/controller/UserService.java
+++ booster-app/src/main/java/de/neuefische/boosterapp/controller/UserService.java
@@ -2,6 +2,7 @@ package de.neuefische.boosterapp.controller;
 
 import de.neuefische.boosterapp.db.UserDb;
 import de.neuefische.boosterapp.model.BoosterUser;
+import de.neuefische.boosterapp.utils.IdUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -10,16 +11,20 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserDb userDb;
+    private final IdUtils idUtils;
     BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
     @Autowired
-    public UserService(UserDb userDb) {
+    public UserService(UserDb userDb , IdUtils idUtils) {
         this.userDb = userDb;
+        this.idUtils = idUtils;
     }
 
     public void register (BoosterUser user){
-        String encodedPw = encoder.encode(user.getPassword());
-        user.setPassword(encodedPw);
+        String codedPw = encoder.encode(user.getPassword());
+        user.setPassword(codedPw);
+        user.setId(idUtils.generateRandomId());
+        user.setRole("user");
         userDb.save(user);
     }
 }
--- booster-app/src/main/java/de/neuefische/boosterapp/model/BoosterUser.java
+++ booster-app/src/main/java/de/neuefische/boosterapp/model/BoosterUser.java
@@ -6,17 +6,22 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import javax.validation.constraints.Size;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Document(collection = "boosterUser")
 public class BoosterUser {
 
+    @Size(min = 5, message = "username min length 5")
     @Id
+    private String id;
     private String username;
+    @Size(min = 5, message = "password min length 5")
     private String password;
     private String firstName;
     private String email;
-    private String user;
+    private String role;
 }
 
