4d43978b88db520e9f47aa81b790d5279ce18ba7
christophersiem <cman1609@gmail.com>
2020-07-24 09:21:45 +0200

    Fix ReactRoutingForwarding

--- booster-app/src/main/java/de/neuefische/boosterapp/controller/ReactRoutingForwarding.java
+++ booster-app/src/main/java/de/neuefische/boosterapp/controller/ReactRoutingForwarding.java
@@ -1,4 +1,4 @@
-package de.lunchbreakapp.lunchbreakbackend.controller;
+package de.neuefische.boosterapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
