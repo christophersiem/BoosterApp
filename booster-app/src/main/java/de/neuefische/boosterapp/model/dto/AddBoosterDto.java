8b132ca3f4cf34af400c9707a925c00353efa2fe
christophersiem <cman1609@gmail.com>
2020-07-27 18:16:36 +0200

    Fix Dtos

--- booster-app/src/main/java/de/neuefische/boosterapp/model/dto/AddBoosterDto.java
+++ booster-app/src/main/java/de/neuefische/boosterapp/model/dto/AddBoosterDto.java
@@ -5,8 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.Size;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -14,12 +12,12 @@ public class AddBoosterDto {
 
     private BoosterType type;
     private String creator;
-    private String creatorName;
+    private String creatorUsername;
+    private String creatorFirstName;
     private String owner;
     private String name;
     private String message;
     private String youtubeLink;
-    private String spotifyLink;
     private String image;
 
 }
--- booster-app/src/main/java/de/neuefische/boosterapp/model/dto/FriendListDto.java
+++ booster-app/src/main/java/de/neuefische/boosterapp/model/dto/FriendListDto.java
@@ -1,18 +1,15 @@
 package de.neuefische.boosterapp.model.dto;
 
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class UserDataDto {
+public class FriendListDto {
 
-    private int createdBooster;
-    private List friends;
-    private String friendsIds;
+    private String userName;
+    private String friend;
 }
-
