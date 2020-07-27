3962b7a1e29f664a52141a1abccccea419b5239f
christophersiem <cman1609@gmail.com>
2020-07-27 11:48:10 +0200

    Remove unnecessary findFriendsByUsername

--- booster-app/src/main/java/de/neuefische/boosterapp/db/UserDb.java
+++ booster-app/src/main/java/de/neuefische/boosterapp/db/UserDb.java
@@ -4,11 +4,8 @@ package de.neuefische.boosterapp.db;
 import de.neuefische.boosterapp.model.BoosterUser;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-import java.util.List;
-
 public interface UserDb extends PagingAndSortingRepository<BoosterUser,String> {
-    BoosterUser findByUsername(String username);
-    List <String> findFriendsByUsername(String userId);
+     BoosterUser findByUsername(String username);
 
     }
 
