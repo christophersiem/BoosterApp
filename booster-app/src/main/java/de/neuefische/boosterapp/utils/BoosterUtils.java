94b72f59053c9029f3d657cc446d1b5c2c84fd5f
christophersiem <cman1609@gmail.com>
2020-07-30 12:32:12 +0200

    Layout

--- booster-app/src/main/java/de/neuefische/boosterapp/utils/BoosterUtils.java
+++ booster-app/src/main/java/de/neuefische/boosterapp/utils/BoosterUtils.java
@@ -24,7 +24,6 @@ public class BoosterUtils {
     }
 
 
-
     public static String getYoutubeId(String url) {
         String pattern = "https?://(?:[0-9A-Z-]+\\.)?(?:youtu\\.be/|youtube\\.com\\S*[^\\w\\-\\s])([\\w\\-]{11})(?=[^\\w\\-]|$)(?![?=&+%\\w]*(?:['\"][^<>]*>|</a>))[?=&+%\\w]*";
 
@@ -37,11 +36,11 @@ public class BoosterUtils {
         return null;
     }
 
-    public void createStandardBooster(String ownerId){
+    public void createStandardBooster(String ownerId) {
 
-        boosterMongoDb.save((new Booster(idUtils.generateRandomId(), JOY, "1b764dc5-9bf2-455c-9563-6ff54d0b5b9d", "MoodBoost", ownerId, "Smile", "Life is short. Smile while you still have teeth :-)", "ekIMGAmgXSI","https://www.rd.com/wp-content/uploads/2019/09/GettyImages-621924830.jpg")));
+        boosterMongoDb.save((new Booster(idUtils.generateRandomId(), JOY, "1b764dc5-9bf2-455c-9563-6ff54d0b5b9d", "MoodBoost", ownerId, "Smile", "Life is short. Smile while you still have teeth :-)", "ekIMGAmgXSI", "https://www.rd.com/wp-content/uploads/2019/09/GettyImages-621924830.jpg")));
         boosterMongoDb.save((new Booster(idUtils.generateRandomId(), CALM, "1b764dc5-9bf2-455c-9563-6ff54d0b5b9d", "MoodBoost", ownerId, "Relax", "“You are the sky. Everything else – it’s just the weather. (Pema Chödrön)” ", "7maJOI3QMu0", "https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1653&q=80")));
-        boosterMongoDb.save((new Booster(idUtils.generateRandomId(), CONFIDENCE, "1b764dc5-9bf2-455c-9563-6ff54d0b5b9d", "MoodBoost", ownerId, "Focus", "Confidence awakens Confidence.", "u_ktRTWMX3M","https://images.unsplash.com/photo-1545281011-95796d6f27ba?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1652&q=80")));
+        boosterMongoDb.save((new Booster(idUtils.generateRandomId(), CONFIDENCE, "1b764dc5-9bf2-455c-9563-6ff54d0b5b9d", "MoodBoost", ownerId, "Focus", "Confidence awakens Confidence.", "u_ktRTWMX3M", "https://images.unsplash.com/photo-1545281011-95796d6f27ba?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1652&q=80")));
     }
 
 
