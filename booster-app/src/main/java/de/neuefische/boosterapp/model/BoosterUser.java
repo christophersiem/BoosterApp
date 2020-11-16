1557355fe2460f5c90cd7afb5b857aca4089d094
christophersiem <christopher.siem@gmail.com>
2020-11-16 21:35:35 +0100

    Add Builder for simplifying tests

--- booster-app/src/main/java/de/neuefische/boosterapp/model/BoosterUser.java
+++ booster-app/src/main/java/de/neuefische/boosterapp/model/BoosterUser.java
@@ -1,6 +1,7 @@
 package de.neuefische.boosterapp.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
@@ -13,6 +14,7 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @Document(collection = "boosterUser")
 public class BoosterUser {
 
